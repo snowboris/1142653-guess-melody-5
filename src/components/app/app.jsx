58da43adbc8fb0f11d2d34ab4e50492296edc13c
Boris Olenev <olenevboris95@yandex.ru>
2020-10-11 21:51:19 +0700

    Added routing

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,10 +1,38 @@
 import React from 'react';
 import WelcomePage from '../welcome-page/welcome-page';
 import PropTypes from 'prop-types';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import Login from '../login/login';
+import Result from '../result/result';
+import Lose from '../lose/lose';
+import DevArtist from '../dev-artist/dev-artist';
+import DevGenre from '../dev-genre/dev-genre';
 
 const App = (props) => {
   return (
-    <WelcomePage settings={props.settings} />
+    <BrowserRouter>
+      <Switch>
+        <Route path='/' exact>
+          <WelcomePage settings={props.settings} />
+        </Route>
+        <Route path='/login'>
+          <Login />
+        </Route>
+        <Route path='/result' >
+          <Result />
+        </Route>
+        <Route path='/lose' >
+          <Lose />
+        </Route>
+        <Route path='/dev-artist' >
+          <DevArtist />
+        </Route>
+        <Route path='/dev-genre' >
+          <DevGenre />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+
   );
 };
 
